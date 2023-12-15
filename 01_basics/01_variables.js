7d96f79b50cf274e82bac7a9f7ee8be78864cd01
Assasineagle <138393198+Assasineagle@users.noreply.github.com>
2023-12-15 10:31:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=24
+let  account_email="anushishandsome@123"
+accountcity="Lucknow"
+accountid=24
+// accountId=36
+account_email= 33
+console.log(accountid);
+console.table([accountid,account_email,accountcity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert(3,4) cant use this as were using nodejs not browser
+
+console.log(23);
+console.log("bnlsd");
+
+let age=34
+let isloggedin=false
+let sirname=null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+console.table([typeof sirname,typeof age,typeof isloggedin]);
+// console.log(typeof age);
+// console.log(typeof isloggedin);
