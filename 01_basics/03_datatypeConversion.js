dfbaeeed891746f99887dc793b2416015a4da24f
Assasineagle <138393198+Assasineagle@users.noreply.github.com>
2023-12-16 15:16:14 +0530

    Add files via upload
    commit

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,27 @@
+let score=false
+
+let numberval= Number(score)
+console.log(numberval);
+console.log(typeof numberval);
+console.log(typeof score);
+// let isLoggedIn="anusheel"   // gives true
+let isLoggedIn=""      // gives false
+console.log(Boolean(isLoggedIn));
+
+console.log(String(numberval))
+
+// console. log (2+2);
+// console. log (2-2);
+// console. log (2*2);
+// console. log (2**3);
+// console. log (2/3);
+// console. log (2%3);
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+
+console.log(str3);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(2+2+"2");
+console.log(2+"2");
